81a607ee05c442b772a7022937867a92f2adae21
Simon Lundström <github-commits@soy.se>
2025-03-08 22:36:11 +0100

    Add first argument, a file!
    Well, it's not even a file.. It's a string. And we won't even use it I
    think.
    
    Time for some README Driven Development!

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,11 @@
-let () = print_endline Miniflux_opml_backup.v
+open Cmdliner
+let main file = Printf.printf "%s says: %s\n%!" file Miniflux_opml_backup.v
+
+let file =
+  let doc = "OPML-file to validate" in
+  Arg.(required & (* FIXME: Make this a file *) opt (some string) None & info ["f"; "file"] ~docv:"FILE" ~doc)
+
+let () = exit (Cmd.eval (
+    let info = Cmd.info "miniflux_opml_backup" ~version:"%%VERSION%%" ~exits: [] in
+    Cmd.v info Term.(const main $ file)
+  ))
