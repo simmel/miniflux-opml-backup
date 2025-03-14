71b79d666b36fe7b73f564cca34dce300ae754f2
Simon Lundström <github-commits@soy.se>
2025-03-14 19:33:02 +0100

    URL argument should be an Uri

--- bin/main.ml
+++ bin/main.ml
@@ -1,5 +1,10 @@
 open Cmdliner
-let main auth_token url = Printf.printf "Using %s to access %s says: %s\n%!" auth_token url Miniflux_opml_backup.v
+
+let main auth_token url =
+  let purl = match url with
+    | Some v -> Uri.to_string v
+    | None -> "" in
+  Printf.printf "Using %s to access %s says: %s\n%!" auth_token purl Miniflux_opml_backup.v
 
 let auth_token =
   let doc = "API key/token to Miniflux" in
@@ -8,9 +13,18 @@ let auth_token =
   in
   Arg.(required & opt (some string) None & info ["auth-token"] ~env ~docv:"API-key" ~doc)
 
+let parse_uri s =
+  match Uri.of_string s with
+  | uri -> Ok uri
+let some_uri s =
+  match Uri.of_string s with
+  | uri -> Some uri
+
+let uri = Arg.conv (parse_uri, Uri.pp_hum)
+
 let url =
   let doc = "URL to Miniflux" in
-  Arg.(value & opt (* FIXME Make this an Uri type *) string "https://reader.miniflux.app/v1/" & info ["url"] ~docv:"URL" ~doc)
+  Arg.(value & opt (some uri) (some_uri "https://reader.miniflux.app/v1/")  & info ["url"] ~docv:"URL" ~doc)
 
 let () = exit (Cmd.eval (
     let info = Cmd.info "miniflux_opml_backup" ~version:"%%VERSION%%" ~exits: [] in
